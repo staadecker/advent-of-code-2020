ffaca631ad0f9fad602b5cbd88e8ccb02f0815d5
Martin Staadecker <machstg@gmail.com>
2020-12-17 11:09:38 -0500

    Switch to 4d

--- 17/main.py
+++ 17/main.py
@@ -1,37 +1,39 @@
 import numpy as np
 
-FILENAME = "sample_input.txt"
+FILENAME = "input.txt"
 
 class NeighbourIterator:
     def __init__(self, pos):
         self.pos = pos
-        self.offset = (0, 0, 0)
-        self.z = None
+        self.offset = None
 
     def increment_offset(self):
-        oz, ox, oy = self.offset
+        ow, oz, ox, oy = self.offset
+
         oy = (oy + 1) % 3
         if oy == 0:
             ox = (ox + 1) % 3
             if ox == 0:
                 oz = (oz + 1) % 3
                 if oz == 0:
-                    self.offset = None
-                    return
+                    ow = (ow + 1) % 3
+                    if ow == 0:
+                        self.offset = None
+                        return
 
-        self.offset = (oz, ox, oy)
+        self.offset = (ow, oz, ox, oy)
 
         # Redo if the offset is the central cell
-        if self.offset == (1, 1, 1):
+        if self.offset == (1, 1, 1, 1):
             self.increment_offset()
 
     def get_pos(self):
-        oz, ox, oy = self.offset
-        z, x, y = self.pos
-        return z - 1 + oz, x - 1 + ox, y - 1 + oy
+        ow, oz, ox, oy = self.offset
+        w, z, x, y = self.pos
+        return w - 1 + ow, z - 1 + oz, x - 1 + ox, y - 1 + oy
 
     def __iter__(self):
-        self.offset = (0, 0, 0)
+        self.offset = (0, 0, 0, 0)
         return self
 
     def __next__(self):
@@ -43,24 +45,28 @@ class NeighbourIterator:
 
 
 class Grid:
-    def __init__(self, height, width, depth, dtype=bool, default=False):
+    def __init__(self, height, width, depth, w_depth=1 + 6 * 2, dtype=bool, default=False):
         self.height = height
         self.width = width
         self.depth = depth
-        self.pos = None  # [z,x,y]
-        self.grid = np.array([[[default] * self.width for _ in range(self.height)] for _ in range(self.depth)],
+        self.w_depth = w_depth
+        self.pos = None  # [w,z,x,y]
+        self.grid = np.array([[[[default] * self.width for _ in range(self.height)] for _ in range(self.depth)] for _ in
+                              range(self.w_depth)],
                              dtype=dtype)
 
     def next_pos(self, pos):
-        (z, x, y) = pos
+        (w, z, x, y) = pos
         y = (y + 1) % self.width
         if y == 0:
             x = (x + 1) % self.height
             if x == 0:
                 z = (z + 1) % self.depth
                 if z == 0:
-                    return None
-        return (z, x, y)
+                    w = (w + 1) % self.w_depth
+                    if w == 0:
+                        return None
+        return w, z, x, y
 
     def apply_swap(self, other):
         self.grid = np.bitwise_xor(self.grid, other.grid)
@@ -73,7 +79,7 @@ class Grid:
         return count
 
     def __iter__(self):
-        self.pos = (0, 0, 0)
+        self.pos = (0, 0, 0, 0)
         return self
 
     def __next__(self):
@@ -91,13 +97,14 @@ class Grid:
 
     def __str__(self):
         out = ""
-        for z, layer in enumerate(self.grid):
-            if np.any(layer):  # Check if anything in the layer is active
-                out += f"z={z - 6}\n"
-                for row in layer:
-                    for c in row:
-                        out += str(int(c) if int(c) > 0 else ".")
-                    out += "\n"
+        for w, w_layer in enumerate(self.grid):
+            for z, layer in enumerate(w_layer):
+                if np.any(layer):  # Check if anything in the layer is active
+                    out += f"w={w-6} & z={z - 6}\n"
+                    for row in layer:
+                        for c in row:
+                            out += str(int(c) if int(c) > 0 else ".")
+                        out += "\n"
         return out
 
     def __repr__(self):
@@ -116,7 +123,7 @@ grid = Grid(height, width, depth)
 # Populate grid with initial values
 for x, row in enumerate(raw_input, start=6):
     for y, char in enumerate(row, start=6):
-        grid[6, x, y] = char == "#"
+        grid[6, 6, x, y] = char == "#"
 
 # Do 6 passes
 for _ in range(6):
